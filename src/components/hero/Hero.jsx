90965b4bb95cffb1defd0fb14b47eca926a14501
Ahmad Mustapha <ahmadishola12@gmail.com>
2024-12-24 11:44:23 -0800

    fixes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -47,32 +47,32 @@ function Hero() {
             <div className='flex items-center justify-center sm:justify-start flex-wrap gap-2 hero-content my-6'>
               <button 
                 onClick={() =>setActiveSport('All sports')}
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'All sports' ? 'bg-orange-700 text-white': ''}`}>All sports</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'All sports' ? 'bg-orange-700 text-white': 'bg-white'}`}>All sports</button>
                 <button 
                 onClick={() =>setActiveSport('tennis')}
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'tennis' ? 'bg-orange-700 text-white': ''}`}><IoMdTennisball className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'tennis' ? 'text-white': ''}`}/> Tennis</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'tennis' ? 'bg-orange-700 text-white': 'bg-white'}`}><IoMdTennisball className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'tennis' ? 'text-white': ''}`}/> Tennis</button>
                 <button
                 onClick={() =>setActiveSport('padel')} 
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'padel'? 'bg-orange-700 text-white': ''}`}><GiTennisRacket className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'padel' ? 'text-white': ''}`}/> Padel</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'padel'? 'bg-orange-700 text-white': 'bg-white'}`}><GiTennisRacket className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'padel' ? 'text-white': ''}`}/> Padel</button>
                 <button
                 onClick={() =>setActiveSport('squash')} 
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'squash' ? 'bg-orange-700 text-white': ''}`}><MdOutlineSportsBaseball  className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'squash' ? 'text-white': ''}`}/>Squash</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'squash' ? 'bg-orange-700 text-white': 'bg-white'}`}><MdOutlineSportsBaseball  className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'squash' ? 'text-white': ''}`}/>Squash</button>
                 <button
                 onClick={() =>setActiveSport('pickleball')} 
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'pickleball' ? 'bg-orange-700 text-white': ''}`}><GiTennisRacket className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'pickleball' ? 'text-white': ''}`}/>Pickleball</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'pickleball' ? 'bg-orange-700 text-white': 'bg-white'}`}><GiTennisRacket className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'pickleball' ? 'text-white': ''}`}/>Pickleball</button>
                 <button
                 onClick={() =>setActiveSport('football')} 
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'football' ? 'bg-orange-700 text-white': ''}`}><IoMdFootball className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'football' ? 'text-white': ''}`}/> Football</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'football' ? 'bg-orange-700 text-white': 'bg-white'}`}><IoMdFootball className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'football' ? 'text-white': ''}`}/> Football</button>
                 <button
                 onClick={() =>setActiveSport('basketball')} 
-                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] bg-white text-[#d94b18] rounded-[100px] 
-                ${activeSport === 'basketball' ? 'bg-orange-700 text-white': ''}`}><FaBasketball  className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'basketball' ? 'text-white': ''}`}/> Basketball</button>
+                className={`flex items-center gap-1 border-[1px] border-[#d94b18] py-[.6rem] px-[.6rem] text-[1.1rem] font-[500] text-[#d94b18] rounded-[100px] 
+                ${activeSport === 'basketball' ? 'bg-orange-700 text-white': 'bg-white'}`}><FaBasketball  className={`text-[#d94b18] text-[1.2rem] ${activeSport === 'basketball' ? 'text-white': ''}`}/> Basketball</button>
             </div>
           <form action="">
             <div className='flex items-center flex-col lg:flex-row gap-8 mt-2 hero-content'>
